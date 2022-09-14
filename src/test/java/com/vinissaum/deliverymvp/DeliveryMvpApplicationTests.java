c86235f78fa5d6f5da183860b894e5472d0a1de5
Vinicius Braz <vinicius_lsb@live.com>
2022-09-14 14:26:53 -0300

    init project

---
+++ src/test/java/com/vinissaum/deliverymvp/DeliveryMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinissaum.deliverymvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

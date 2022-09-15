bd2bc19717f424fdcf552e90c15347fe6bc9e008
Vinicius Braz <vinicius_lsb@live.com>
2022-09-15 11:32:28 -0300

    feat: seed restaurant table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO kitchen (name) VALUES ('Tailandesa')
 INSERT INTO kitchen (name) VALUES ('Indiana')
+
+INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ('Indian foodtruck', 5.00, 2)
+INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ('Sasa Kamar gourmet', 7.00, 1)

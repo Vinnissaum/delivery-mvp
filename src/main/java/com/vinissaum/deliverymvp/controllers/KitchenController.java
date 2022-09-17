26efb02b06d744ff9f37e1ed6d99c0b0b12f40b7
Vinicius Braz <vinicius_lsb@live.com>
2022-09-17 14:29:26 -0300

    feat: add produces to specify the accepted content type

--- src/main/java/com/vinissaum/deliverymvp/controllers/KitchenController.java
+++ src/main/java/com/vinissaum/deliverymvp/controllers/KitchenController.java
@@ -3,6 +3,7 @@ package com.vinissaum.deliverymvp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import com.vinissaum.deliverymvp.domain.model.Kitchen;
 import com.vinissaum.deliverymvp.domain.repositories.KitchenRepository;
 
 @RestController
-@RequestMapping("/kitchens")
+@RequestMapping(value = "/kitchens", produces = MediaType.APPLICATION_JSON_VALUE)
 public class KitchenController {
 
     @Autowired

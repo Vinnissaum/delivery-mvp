10c71529848ad732acf7343165eacc92dcf426ca
Vinicius Braz <vinicius_lsb@live.com>
2022-09-17 22:35:55 -0300

    feat: add post method

--- src/main/java/com/vinissaum/deliverymvp/controllers/KitchenController.java
+++ src/main/java/com/vinissaum/deliverymvp/controllers/KitchenController.java
@@ -3,10 +3,15 @@ package com.vinissaum.deliverymvp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vinissaum.deliverymvp.domain.model.Kitchen;
@@ -27,5 +32,12 @@ public class KitchenController {
     @GetMapping("/{id}")
     public Kitchen show(@PathVariable Long id) {
         return repository.find(id);
+    }
+
+    @ResponseStatus(code = HttpStatus.CREATED)
+    @PostMapping
+    public Kitchen store(@RequestBody Kitchen kitchen) {
+
+        return repository.insert(kitchen);
     }
 }
--- src/main/java/com/vinissaum/deliverymvp/controllers/StateController.java
+++ src/main/java/com/vinissaum/deliverymvp/controllers/StateController.java
@@ -3,9 +3,13 @@ package com.vinissaum.deliverymvp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vinissaum.deliverymvp.domain.model.State;
@@ -26,5 +30,12 @@ public class StateController {
     @GetMapping("/{id}")
     public State show(@PathVariable Long id) {
         return repository.find(id);
+    }
+
+    @ResponseStatus(code = HttpStatus.CREATED)
+    @PostMapping
+    public State store(@RequestBody State state) {
+
+        return repository.insert(state);
     }
 }

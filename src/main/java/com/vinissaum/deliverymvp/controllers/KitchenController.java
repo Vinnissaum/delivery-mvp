873248cfbc7da0be28bdbe0bf8d95be20c6913c7
Vinicius Braz <vinicius_lsb@live.com>
2022-09-17 21:42:58 -0300

    feat: add find using path variable

--- src/main/java/com/vinissaum/deliverymvp/controllers/KitchenController.java
+++ src/main/java/com/vinissaum/deliverymvp/controllers/KitchenController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class KitchenController {
     @GetMapping
     public List<Kitchen> index() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Kitchen show(@PathVariable Long id) {
+        return repository.find(id);
     }
 }
--- src/main/java/com/vinissaum/deliverymvp/controllers/StateController.java
+++ src/main/java/com/vinissaum/deliverymvp/controllers/StateController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class StateController {
     @GetMapping
     public List<State> index() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public State show(@PathVariable Long id) {
+        return repository.find(id);
     }
 }

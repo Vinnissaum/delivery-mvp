4f64936cbd6f5c54308e87899ed5a7d7b445bdf1
Vinicius Braz <vinicius_lsb@live.com>
2022-10-04 20:18:17 -0300

    feat: add a address class as embeddable

---
+++ src/main/java/com/vinissaum/deliverymvp/domain/model/Address.java
@@ -0,0 +1,33 @@
+package com.vinissaum.deliverymvp.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Data
+@Embeddable
+public class Address {
+
+    @Column(name = "address_zip_code")
+    private String zipCode;
+
+    @Column(name = "address_street")
+    private String street;
+
+    @Column(name = "address_number")
+    private String number;
+
+    @Column(name = "address_complement")
+    private String complement;
+
+    @Column(name = "address_district")
+    private String district;
+
+    @ManyToOne
+    @JoinColumn(name = "address_city")
+    private City city;
+
+}

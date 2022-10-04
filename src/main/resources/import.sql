4bd12dbd97b178ba476cf7b5e63e9b04d4b6c409
Vinicius Braz <vinicius_lsb@live.com>
2022-10-04 20:36:05 -0300

    feat:  add created  and  updated at to restaurant

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,8 +7,8 @@ INSERT INTO state (name) VALUES ('RJ');
 INSERT INTO city (name, state_id) VALUES ('Sao Lourenco', 1);
 INSERT INTO city (name, state_id) VALUES ('Paraty', 2);
 
-INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ('Indian foodtruck', 5.00, 2);
-INSERT INTO restaurant (name, shipping_fee, kitchen_id, address_zip_code, address_district, address_street, address_number, address_complement, address_city) VALUES ('Sasa Kamar gourmet', 7.00, 1, "12222-222", "Sunset", "My Sunshine", "2806", "Ap: 2002", 1);
+INSERT INTO restaurant (name, shipping_fee, kitchen_id, created_at, updated_at) VALUES ('Indian foodtruck', 5.00, 2, utc_timestamp, utc_timestamp);
+INSERT INTO restaurant (name, shipping_fee, kitchen_id, address_zip_code, address_district, address_street, address_number, address_complement, address_city, created_at, updated_at) VALUES ('Sasa Kamar gourmet', 7.00, 1, "12222-222", "Sunset", "My Sunshine", "2806", "Ap: 2002", 1,  utc_timestamp, utc_timestamp);
 
 INSERT INTO payment_type (description) VALUES ('credit card'), ('debit card'), ('money');
 

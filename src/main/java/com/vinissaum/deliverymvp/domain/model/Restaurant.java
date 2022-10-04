9d243e53edb2bb78b4fb893dde8383486c863df6
Vinicius Braz <vinicius_lsb@live.com>
2022-10-04 19:45:15 -0300

    feat: add restaurant paymenttype relationship and db seeding

--- src/main/java/com/vinissaum/deliverymvp/domain/model/Restaurant.java
+++ src/main/java/com/vinissaum/deliverymvp/domain/model/Restaurant.java
@@ -1,11 +1,16 @@
 package com.vinissaum.deliverymvp.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -24,4 +29,11 @@ public class Restaurant {
 
     @ManyToOne
     private Kitchen kitchen;
+
+    @ManyToMany
+    @JoinTable(name = "restaurant_payment_type",
+        joinColumns = @JoinColumn(name = "restaurant_id"),
+        inverseJoinColumns = @JoinColumn(name = "payment_type_id")
+    )
+    private List<PaymentType> paymentTypes = new ArrayList<>();
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,17 @@
-INSERT INTO kitchen (name) VALUES ('Tailandesa')
-INSERT INTO kitchen (name) VALUES ('Indiana')
+INSERT INTO kitchen (name) VALUES ('Tailandesa');
+INSERT INTO kitchen (name) VALUES ('Indiana');
 
-INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ('Indian foodtruck', 5.00, 2)
-INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ('Sasa Kamar gourmet', 7.00, 1)
+INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ('Indian foodtruck', 5.00, 2);
+INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ('Sasa Kamar gourmet', 7.00, 1);
 
-INSERT INTO state (name) VALUES ('MG')
-INSERT INTO state (name) VALUES ('RJ')
+INSERT INTO state (name) VALUES ('MG');
+INSERT INTO state (name) VALUES ('RJ');
 
-INSERT INTO city (name, state_id) VALUES ('Sao Lourenco', 1)
-INSERT INTO city (name, state_id) VALUES ('Paraty', 2)
+INSERT INTO city (name, state_id) VALUES ('Sao Lourenco', 1);
+INSERT INTO city (name, state_id) VALUES ('Paraty', 2);
 
-INSERT INTO payment_type (description) VALUES ('credit card')
+INSERT INTO payment_type (description) VALUES ('credit card'), ('debit card'), ('money');
 
-INSERT INTO permission (name, description) VALUES ('admin', 'grant all permissions')
+INSERT INTO permission (name, description) VALUES ('admin', 'grant all permissions');
+
+INSERT INTO restaurant_payment_type (restaurant_id, payment_type_id) VALUES (2, 1), (2, 2), (1, 3);

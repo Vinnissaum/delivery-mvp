fc4eb7e6a3be1bd777a940198113bd9146161d45
Vinicius Braz <vinicius_lsb@live.com>
2022-12-27 16:30:04 -0300

    feat: add custom jpql query avoiding multiple selects at eager fetch type

--- src/main/java/com/vinissaum/deliverymvp/domain/repositories/RestaurantRepository.java
+++ src/main/java/com/vinissaum/deliverymvp/domain/repositories/RestaurantRepository.java
@@ -3,7 +3,13 @@ package com.vinissaum.deliverymvp.domain.repositories;
 import com.vinissaum.deliverymvp.domain.model.Restaurant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {}
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    @Query("from Restaurant r join r.kitchen left join fetch r.paymentTypes")
+    List<Restaurant> findAll();
+}

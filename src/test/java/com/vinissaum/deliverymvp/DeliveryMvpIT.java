c96c4264293e070695326423a1fbc16e541805e6
Vinicius Braz <vinicius_lsb@live.com>
2023-03-10 12:14:05 -0300

    build: add maven failsafe to prevent integration tests run on build

--- src/test/java/com/vinissaum/deliverymvp/DeliveryMvpIT.java
+++ src/test/java/com/vinissaum/deliverymvp/DeliveryMvpIT.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-class DeliveryMvpApplicationTests {
+class DeliveryMvpIT {
 
     @Autowired
     private KitchenService kitchenService;
